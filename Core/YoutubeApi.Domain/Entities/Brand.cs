71e5389fafa3d72555bdef32ceb122a5502d986e
serdarelsinao <serdarsina2861@gmail.com>
2025-07-03 11:13:45 +0300

    temel entitiyler oluşturuldu

---
+++ Core/YoutubeApi.Domain/Entities/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+    }
+}
