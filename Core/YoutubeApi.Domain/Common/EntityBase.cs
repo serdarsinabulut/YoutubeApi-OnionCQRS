262464f98a5d4cad39ed992eea494811bd5ddc13
serdarelsinao <serdarsina2861@gmail.com>
2025-07-04 09:16:14 +0300

    dbcontext oluşturuldu.

--- Core/YoutubeApi.Domain/Common/EntityBase.cs
+++ Core/YoutubeApi.Domain/Common/EntityBase.cs
@@ -9,7 +9,7 @@ namespace YoutubeApi.Domain.Common
     public class EntityBase : IEntityBase
     {
         public int Id { get; set; }
-        public DateTime dateTime { get; set; } = DateTime.Now;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
         public bool IsDeleted { get; set; } = false;
     }
 }
