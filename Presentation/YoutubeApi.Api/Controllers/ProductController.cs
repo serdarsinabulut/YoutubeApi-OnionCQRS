50af900bdf7d51965647d6f7b6e39bf3870d9f7e
serdarelsinao <serdarsina2861@gmail.com>
2025-07-28 21:31:54 +0300

    cqrs giriş

---
+++ Presentation/YoutubeApi.Api/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeApi.Application.Features.Products.Queries.GetAllProducts;
+
+namespace YoutubeApi.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var response = await mediator.Send(new GetAllProductsQueryRequest());
+
+            return Ok();
+        }
+    }
+}
